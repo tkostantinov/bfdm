8a46c2f5a326efeac34e03c91e6377a05e8a3b8d
User <tkostantinov@users.noreply.github.com>
2018-03-07 11:25:26 +0100

    initial commit

---
+++ src/BFMD/Parser.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace BDFM;
+
+
+class Parser
+{
+    public function parse($input)
+    {
+        $output = "";
+
+        //@TODO parsing algorithm
+
+        return $output;
+    }
+}
